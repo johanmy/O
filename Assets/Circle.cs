e55bdc52a113c0a234674f9fab77d350bbb43bce
johanmy <johanmy@users.noreply.github.com>
2025-05-28 05:25:26 +0800

    Rigidbody2D

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -1,4 +1,11 @@
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
 class Circle : MonoBehaviour
 {
+    void Start()
+    {
+        Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rigidbody2D.gravityScale = 0;
+    }
 }

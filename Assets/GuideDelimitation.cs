e842d713a065d53143700493f7f9ee70e7027d64
johanmy <johanmy@users.noreply.github.com>
2025-05-28 15:56:16 +0800

    GuideDelimitation

---
+++ Assets/GuideDelimitation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+class GuideDelimitation : MonoBehaviour, IPointerDownHandler
+{
+    Transform guideOrientation;
+    void Start()
+    {
+        guideOrientation = transform.GetChild(0);
+    }
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        guideOrientation.position = Mouse.current.position.value;
+    }
+}
